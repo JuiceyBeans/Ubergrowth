b3e92c63ee5a823f172725c96143eb2c9b0080b3
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-07-06 04:03:27 +0100

    Initial commit

---
+++ src/client/java/com/juiceybeans/ubergrowth/client/UbergrowthClient.java
@@ -0,0 +1,10 @@
+package com.juiceybeans.ubergrowth.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UbergrowthClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
