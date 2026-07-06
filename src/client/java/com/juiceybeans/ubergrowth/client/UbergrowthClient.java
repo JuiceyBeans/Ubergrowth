ffe86e8a3e57eb88c1af3b9eec14842c3fa05841
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-07-06 06:24:25 +0100

    Set render layers for rust, dust, and grime to translucent

--- src/client/java/com/juiceybeans/ubergrowth/client/UbergrowthClient.java
+++ src/client/java/com/juiceybeans/ubergrowth/client/UbergrowthClient.java
@@ -6,11 +6,21 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 
+import java.util.Set;
+
 public class UbergrowthClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+        Set<Block> translucentBlocks = Set.of(
+                UbergrowthBlocks.RUST,
+                UbergrowthBlocks.DUST,
+                UbergrowthBlocks.GRIME
+        );
+
+        translucentBlocks.forEach(block -> BlockRenderLayerMap.INSTANCE.putBlock(block, RenderType.translucent()));
+
         for (Block block : UbergrowthBlocks.blocks) {
-            BlockRenderLayerMap.INSTANCE.putBlock(block, RenderType.cutout());
+            if (!translucentBlocks.contains(block)) BlockRenderLayerMap.INSTANCE.putBlock(block, RenderType.cutout());
         }
 	}
 }
