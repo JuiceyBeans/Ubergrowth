b0c2bf5ebec59b22299625e1acd07d0e5870b78e
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-07-06 04:58:14 +0100

    Recipe datagen

---
+++ src/client/java/com/juiceybeans/ubergrowth/client/datagen/UbergrowthDataGenerator.java
@@ -0,0 +1,13 @@
+package com.juiceybeans.ubergrowth.client.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UbergrowthDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+
+        pack.addProvider(UbergrowthRecipeProvider::new);
+	}
+}
---
+++ src/client/java/com/juiceybeans/ubergrowth/client/datagen/UbergrowthRecipeProvider.java
@@ -0,0 +1,17 @@
+package com.juiceybeans.ubergrowth.client.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.recipes.FinishedRecipe;
+
+import java.util.function.Consumer;
+
+public class UbergrowthRecipeProvider extends FabricRecipeProvider {
+    public UbergrowthRecipeProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void buildRecipes(Consumer<FinishedRecipe> writer) {
+    }
+}
