c323529b9550ca2dc209ebb696b0bb990cbd60de
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-07-06 10:28:18 +0100

    Waterloggable blocks

--- src/main/java/com/juiceybeans/ubergrowth/block/BaseUbergrowthBlock.java
+++ src/main/java/com/juiceybeans/ubergrowth/block/BaseUbergrowthBlock.java
@@ -3,8 +3,9 @@ package com.juiceybeans.ubergrowth.block;
 import net.minecraft.world.level.block.MultifaceBlock;
 import net.minecraft.world.level.block.MultifaceSpreader;
 
-public class UbergrowthBlock extends MultifaceBlock {
-    public UbergrowthBlock(Properties properties) {
+public class BaseUbergrowthBlock extends MultifaceBlock {
+
+    public BaseUbergrowthBlock(Properties properties) {
         super(properties.noOcclusion());
     }
 
